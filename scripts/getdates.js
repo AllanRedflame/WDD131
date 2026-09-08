ba5fe40ad6853273670002787eef018f957744f0
AllanRedflame <Allanredflame@gmail.com>
2026-09-07 20:06:15 -0600

    trying this

--- scripts/getdates.js
+++ scripts/getdates.js
@@ -5,5 +5,3 @@ let copyright = document.getElementById("currentyear");
 
 document.getElementById("lastModified").innerHTML = "Last updated: " + modified;
 copyright.innerHTML = `${year} | David Poulsen | United States`;
-
-console. daw
