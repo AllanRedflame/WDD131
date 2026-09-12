50cdbe162ead8a4f18fadba997b4e16f1f42f275
AllanRedflame <Allanredflame@gmail.com>
2026-09-11 18:22:49 -0600

    update semicolon

--- scripts/getdates.js
+++ scripts/getdates.js
@@ -4,4 +4,4 @@ const modified = document.lastModified;
 let copyright = document.getElementById("currentyear");
 
 document.getElementById("lastModified").innerHTML = "Last updated: " + modified;
-copyright.innerHTML = `&copy${year} | David Poulsen | United States`;
+copyright.innerHTML = `&copy; ${year} | David Poulsen | United States`;
